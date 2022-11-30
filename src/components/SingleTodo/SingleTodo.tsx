fe03ad4d19eb3106ebd4cbf929d97216fa7c7e80
Nikita <lavro.nikita123@gmail.com>
2022-11-29 21:51:48 -0500

    singleTodoComponent

--- src/components/SingleTodo/SingleTodo.tsx
+++ src/components/SingleTodo/SingleTodo.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
+//Styles
+import "./styles.css";
+
+//ReactIcons
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
 //Todo Model
 import { Todo } from "../../model/model";
 
@@ -10,7 +17,22 @@ interface Props {
 }
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  return <div>SingleTodo</div>;
+  return (
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+      <div className="todos__single__icons">
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
