18b2e4453ff23e91191b5f1d9fc5300f95a1d202
Nikita <lavro.nikita123@gmail.com>
2022-11-29 20:38:03 -0500

    create Todo logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 //Styles
 import "./styles.css";
@@ -6,11 +6,12 @@ import "./styles.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className="input">
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
       <input
         type="input"
         value={todo}
